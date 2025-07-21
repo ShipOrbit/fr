8cd78e68dcf85527ddcd617384f6c346e6796cf2
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-21 19:39:26 +0300

    fix: handle redirect on missing auth + shipping needs + not verified

--- src/pages/shippers/components/layout.tsx
+++ src/pages/shippers/components/layout.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
-import Header from "./header";
+import { Loader2 } from "lucide-react";
+import React from "react";
+import { Navigate } from "react-router";
 import { useAuth } from "../../../hooks/use-auth";
-import { useNavigate } from "react-router";
+import Header from "./header";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,16 +10,27 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { user } = useAuth();
-  const navigate = useNavigate();
+  const { isAuthenticated, isLoading, user } = useAuth();
 
-  useEffect(() => {
-    if (!user?.shipping_needs) {
-      navigate("/sign-up-2");
-    } else if (!user.is_email_verified) {
-      navigate("/sign-up-3");
-    }
-  }, [navigate, user]);
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+        <Loader2 className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user?.shipping_needs) {
+    return <Navigate to="/sign-up-2" replace />;
+  }
+
+  if (!user.is_email_verified) {
+    return <Navigate to="/sign-up-3" replace />;
+  }
 
   return (
     <div className="min-h-screen bg-gray-50">
