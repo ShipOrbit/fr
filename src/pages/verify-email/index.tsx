9270515174f35ce59f91687647bf29e7034943fc
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-21 06:32:25 +0300

    fix: after verification link

--- src/pages/verify-email/index.tsx
+++ src/pages/verify-email/index.tsx
@@ -106,7 +106,7 @@ const VerifyEmail: React.FC = () => {
                     className="mx-auto py-2 px-4 text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
                     to="/dashboard"
                   >
-                    Continue To Dashboard
+                    Now Go To Login
                   </Link>
                 )}
               </div>
