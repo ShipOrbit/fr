92ffb18856e221a8ca238a1e07060f6157445018
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-25 08:03:39 +0300

    fix(style): remove bg + shadow

--- src/pages/not-yet/index.tsx
+++ src/pages/not-yet/index.tsx
@@ -5,10 +5,8 @@ export default function NotYetPage() {
   return (
     <Layout>
       <div className="min-h-screen flex items-center justify-center">
-        <div className="bg-white flex flex-col items-center justify-center gap-4 p-4 w-1/2 h-96 rounded-2xl shadow-2xl">
-          <Package size={48} className="animate-pulse" />
-          <h1 className="text-2xl font-bold animate-pulse">Working On It..</h1>
-        </div>
+        <Package size={48} className="animate-pulse" />
+        <h1 className="text-2xl font-bold animate-pulse">Working On It..</h1>
       </div>
     </Layout>
   );
