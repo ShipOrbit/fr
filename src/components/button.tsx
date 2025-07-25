685811c784d4caa74f428ac13f0ab2d9e99056dd
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-25 20:14:49 +0300

    feat: add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,31 @@
+import { Loader2 } from "lucide-react";
+import type { PropsWithChildren } from "react";
+import { cn } from "../utils/cn";
+
+export type ButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  PropsWithChildren & { loading: boolean };
+
+export default function Button({
+  children,
+  disabled,
+  loading,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg font-semibold transition-colors ml-auto",
+        className
+      )}
+      disabled={disabled || loading}
+      {...props}
+    >
+      {loading && <Loader2 className="animate-spin" />}
+      {children}
+    </button>
+  );
+}
