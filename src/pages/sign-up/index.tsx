a809c45cb7cb7bd5e5a5664a3deb58190f0868c7
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-26 15:09:39 +0300

    fix: remove height + padding

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -84,7 +84,7 @@ const SignUpStep1: React.FC = () => {
 
   return (
     <Layout>
-      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-screen px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden">
             <div className="lg:flex">
